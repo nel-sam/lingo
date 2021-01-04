740e8120c7936dd2b093d2cfef0bd20dd4a2d29f
Nelson <nelson.rs.07@gmail.com>
2021-01-03 18:33:17 -0700

    Started data seeding

--- api/Data/GameContext.cs
+++ api/Data/GameContext.cs
@@ -1,3 +1,4 @@
+using System;
 using api.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,5 +14,40 @@ namespace api.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
             => options.UseSqlite("Data Source=lingo.db");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Player>().HasKey(x => new { x.Board.BoardId });
+
+            var player1Board = new Board()
+            {
+                BoardId = 1,
+                Cells = null
+            };
+
+            modelBuilder.Entity<Board>().HasData(player1Board);
+
+            modelBuilder.Entity<Player>().HasData(
+                new Player()
+                {
+                    PlayerId = 1,
+                    FirstName = "Nelson",
+                    LastName = "Samayoa",
+                    Key = new Guid("5d516a2b-9722-481c-b98e-89831ae4a732"),
+                    Board = player1Board
+                }
+            );
+
+            modelBuilder.Entity<Game>().HasData(
+               new Game
+               {
+                    GameId = 1,
+                    Key = new Guid("647c6980-a700-435c-b9c1-4695568a8229"),
+                    Players =
+                }
+            );
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
